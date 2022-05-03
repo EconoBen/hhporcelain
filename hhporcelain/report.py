33da8747dcbe4ea97f4f2353c94ce7a9ae3ab949
Ben Labaschin <blabaschin@Bens-MacBook-Pro.local>
2022-05-03 14:35:38 -0700

    success

--- hhporcelain/report.py
+++ hhporcelain/report.py
@@ -1,91 +1,66 @@
-import gspread_pandas
 from pandas import DataFrame
 import streamlit as st
 from google.oauth2 import service_account
-import json
+from gsheetsdb import connect
 
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.ServiceAccountCredentials.Credentials.from_service_account_info(key_dict)
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
 
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query)
+    df = DataFrame(rows.fetchall())
+    return df
+
+sheet_names = ['Foundation Needs',
+               'Purchased',
+               'Monthly Costs',
+               'Down-the-line extras',
+               ]
+sheet_dict = {f'sheet{i+1}': val for i, val in enumerate(sheet_names)}
 
 def run():
     table_options = st.selectbox("Which table would you like to view?", 
-                    ('Foundation needs'.capitalize(),
-                     'Down-the-line extras'.capitalize(),
-                     'Monthly Costs'.capitalize(),
-                     'Purchased'.capitalize()
-                     )
+                    tuple(sheet_dict.values())
+                     
     )
-    # Starting Budget
-    id = '1XmLtn9rPnowNX1DAeMuHfRbU3g5SfLQp_5SBXMVtMr0'
-    df = gspread_pandas.spread.Spread(id).sheet_to_df().reset_index()
-
-    if table_options == 'Foundation needs'.capitalize():
+    if table_options == sheet_names[0]:
+        sheet_url = st.secrets["gsheets"]['sheet1']
         # Create Table One
+        df = run_query(f'SELECT * FROM "{sheet_url}"')
 
-        table_one_title = df.columns[0]
-        tbl1_rows = [i for i, x in enumerate(df['Foundation Needs'].values) if len(set(x)) == 1][0]
-        tbl_one_unformatted = df.iloc[:tbl1_rows, :1].values
+        st.header(sheet_names[0])
+        st.write(df)
 
-        tbl_one_cols = tbl_one_unformatted[0][0]
-        items = [lst[0][0].strip() for lst in tbl_one_unformatted[1:] if lst[0][0]]
-        links = [lnk[0][1].strip() for lnk in tbl_one_unformatted[1:]][:len(items)]
-        costs = [cst[0][2].strip() for cst in tbl_one_unformatted[1:]][:len(items)]
-        tbl_one = DataFrame().from_dict({tbl_one_cols[0].capitalize(): items,
-                                    tbl_one_cols[1].capitalize(): links,
-                                    tbl_one_cols[2].capitalize(): costs})
+    elif table_options == sheet_names[1]:
+        sheet_url = st.secrets["gsheets"]['sheet2']
+        # Create Table Two
+        df = run_query(f'SELECT * FROM "{sheet_url}"')
 
-        st.header(table_one_title.capitalize())
-        st.write(tbl_one)
-    elif table_options == 'Down-the-line extras'.capitalize():
+        st.header(sheet_names[1])
+        st.write(df)
 
-        # Table Two
-        tbl1_rows = [i for i, x in enumerate(df['Foundation Needs'].values) if len(set(x)) == 1][0]
-        table_two_title = df.iloc[tbl1_rows:tbl1_rows+1,:1].values[0][0][0]
-        tbl_two_unformatted = df.iloc[tbl1_rows+1:, :1].values
+    elif table_options == sheet_names[2]:
+        sheet_url = st.secrets["gsheets"]['sheet3']
+        # Create Table Three
+        df = run_query(f'SELECT * FROM "{sheet_url}"')
+        
+        st.header(sheet_names[2])
+        st.write(df)
 
-        tbl_two_cols = tbl_two_unformatted[0][0]
-        items = [lst[0][0].strip() for lst in tbl_two_unformatted[1:] if lst[0][0]]
-        links = [lnk[0][1].strip() for lnk in tbl_two_unformatted[1:]][:len(items)]
-        costs = [cst[0][2].strip() for cst in tbl_two_unformatted[1:]][:len(items)]
-        tbl_two = DataFrame().from_dict({tbl_two_cols[0].capitalize(): items,
-                                        tbl_two_cols[1].capitalize(): links,
-                                        tbl_two_cols[2].capitalize(): costs})
-
-        st.header(table_two_title.capitalize())
-        st.write(tbl_two)
-    elif table_options == 'Monthly Costs'.capitalize():
-        # Table Three
-
-        tbl_three_title = df.columns[2]
-        tbl_three_range = [ i for i, x in enumerate(df.iloc[:,2:3].values) if x == ''][0]
-        tbl_three_unformatted = df.iloc[:tbl_three_range,2:5]
-        tbl_three_cols = tbl_three_unformatted.iloc[:1,:].values[0]
-        tbl_three_unformatted = tbl_three_unformatted.iloc[1:,].values
-
-        items = [lst[0] for lst in tbl_three_unformatted if lst[0]]
-        costs = [cst[1].strip() for cst in tbl_three_unformatted][:len(items)]
-        freq = [lst[2] for lst in tbl_three_unformatted][:len(items)]
-        tbl_three = DataFrame().from_dict({tbl_three_cols[0].capitalize(): items,
-                                        tbl_three_cols[1].capitalize(): freq,
-                                        tbl_three_cols[2].capitalize(): costs})
-
-        st.header(tbl_three_title.capitalize())
-        st.write(tbl_three)
-
-    elif table_options == 'Purchased'.capitalize():
-        # Table Four
-        tbl_three_range = [ i for i, x in enumerate(df.iloc[:,2:3].values) if x == ''][0]
-        tbl_four_range = [i+1 for i, x in enumerate(df.iloc[tbl_three_range:,-1:].values) if '$' in x[0]][1]
-        tbl_four_unformatted = df.iloc[tbl_four_range:,2:]
-        tbl_four_title = tbl_four_unformatted.iloc[:1,:].values[0][0]
-        tbl_four_cols = list(set(tbl_four_unformatted.iloc[1:,].values[0]))
-
-        items = [x[0] for x in tbl_four_unformatted.iloc[2:,].values]
-        costs = [x[1] for x in tbl_four_unformatted.iloc[2:,2:].values]
-        tbl_four = DataFrame().from_dict({tbl_four_cols[0].capitalize(): items,
-                                        tbl_four_cols[1].capitalize(): costs
-                                        })
-
-        st.header(tbl_four_title.capitalize())
-        st.write(tbl_four)
+    elif table_options == sheet_names[3]:
+        sheet_url = st.secrets["gsheets"]['sheet4']
+        # Create Table Three
+        df = run_query(f'SELECT * FROM "{sheet_url}"')
+        
+        st.header(sheet_names[3])
+        st.write(df)
